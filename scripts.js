aa90c461e77f82be08f47822d86e839b1515d9c0
Maria Belan-Crawford <44656740+mViolet@users.noreply.github.com>
2020-05-21 22:00:22 -0500

    fix to close fullscreen controls

--- scripts.js
+++ scripts.js
@@ -66,6 +66,19 @@ function startFullScreen() {
     }
 }
 
+/* close fullscreen controls after exit fullscreen */
+function closeFullscreen() {
+	if (document.exitFullscreen) {
+		document.exitFullscreen();
+	} else if (document.mozCancelFullScreen) { /* Firefox */
+		document.mozCancelFullScreen();
+	} else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+		document.webkitExitFullscreen();
+	} else if (document.msExitFullscreen) { /* IE/Edge */
+		document.msExitFullscreen();
+	}
+}
+
 //hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
